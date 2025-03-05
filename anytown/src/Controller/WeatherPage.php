9f8584035f94543426dde53b6675ec6ebc1d07b7
abdessamadtanafaat <tanafaatabdessamad@gmail.com>
2025-03-05 16:04:00 +0000

    weather custom module

---
+++ anytown/src/Controller/WeatherPage.php
@@ -0,0 +1,158 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\anytown\Controller;
+
+use Drupal\anytown\ForecastClientInterface;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Controller for anytown.weather_page route.
+ */
+class WeatherPage extends ControllerBase {
+
+  use StringTranslationTrait;
+  /**
+   * Forecast API client.
+   *
+   * @var \Drupal\anytown\ForecastClientInterface
+   */
+  private $forecastClient;
+
+  /**
+   * WeatherPage controller constructor.
+   *
+   * @param \Drupal\anytown\ForecastClientInterface $forecast_client
+   *   Forecast API client service.
+   */
+  public function __construct(ForecastClientInterface $forecast_client) {
+    $this->forecastClient = $forecast_client;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('anytown.forecast_client')
+    );
+  }
+
+  /**
+   * Builds the response.
+   */
+  public function build(string $style): array {
+    // Style should be one of 'short', or 'extended'. And default to 'short'.
+    $style = (in_array($style, ['short', 'extended'])) ? $style : 'short';
+
+    $url = 'https://raw.githubusercontent.com/DrupalizeMe/module-developer-guide-demo-site/main/backups/weather_forecast.json';
+    $forecast_data = $this->forecastClient->getForecast($url);
+
+    $table_rows = [];
+    $highest = 0;
+    $lowest = 0;
+    if ($forecast_data) {
+      // Create a table of the weather forecast as a render array. First loop
+      // over the forecast data and create rows for the table.
+      foreach ($forecast_data as $item) {
+        [
+          'weekday' => $weekday,
+          'description' => $description,
+          'high' => $high,
+          'low' => $low,
+          'icon' => $icon,
+        ] = $item;
+
+        // Create one table row for each day in the forecast.
+        $table_rows[] = [
+          // Simple text for a cell can be added directly to the array.
+          $weekday,
+          // Complex data for a cell, like HTML, can be represented as a nested
+          // render array.
+          [
+            'data' => [
+              '#markup' => '<img alt="' . $description . '" src="' . $icon . '" width="200" height="200" />',
+            ],
+          ],
+          [
+            'data' => [
+//              '#markup' => "<em>{$description}</em> with a high of {$high} and a low of {$low}",
+              '#markup' => $this->t('%description with a high of @high and a low of @low',
+                [
+                  '%description' => $description,
+                  '@high' => $high,
+                  '@low' => $low,
+                ]
+              ),
+            ],
+          ],
+        ];
+
+        $highest = max($highest, $high);
+        $lowest = min($lowest, $low);
+      }
+
+      // Extended forecast as a table.
+      $weather_forecast = [
+        '#type' => 'table',
+        '#header' => [
+          $this->t('Day'),
+          '',
+          $this->t('Forecast'),
+        ],
+        '#rows' => $table_rows,
+        '#attributes' => [
+          'class' => ['weather_page--forecast-table'],
+        ],
+      ];
+
+      // Summary forecast.
+      $short_forecast = [
+        '#type' => 'markup',
+//        '#markup' => "The high for the weekend is {$highest} and the low is {$lowest}.",
+        '#markup' => '<p>' . $this->t('The high for the weekend is @highest and the low is @lowest.',
+            [
+              '@highest' => $highest,
+              '@lowest' => $lowest,
+            ]
+          ) . '</p>',
+      ];
+
+    }
+    else {
+      // Or, display a message if we can't get the current forecast.
+      $weather_forecast = ['#markup' => $this->t('<p>Could not get the weather forecast. Dress for anything.</p>')];
+      $short_forecast = NULL;
+    }
+
+    $build = [
+      // Which theme hook to use for this content. See anytown_theme().
+      '#theme' => 'weather_page',
+      // When passing a render array to Twig template file any top level array
+      // element that starts with a '#' will be a variable in the template file.
+      // Example: {{ weather_intro }}.
+      '#attached'=> [
+        'library' => ['anytown/forecast'],
+      ],
+    '#weather_intro' => [
+        '#markup' => $this->t("<p>Check out this weekend's weather forecast and come prepared. The market is mostly outside, and takes place rain or shine.</p>"),
+      ],
+      '#weather_forecast' => $weather_forecast,
+      '#short_forecast' => $short_forecast,
+      '#weather_closures' => [
+        '#theme' => 'item_list',
+        '#title' => $this->t('Weather related closures'),
+        '#items' => [
+          $this->t('Ice rink closed until winter - please stay off while we prepare it.'),
+          $this->t('Parking behind Apple Lane is still closed from all the rain last weekend.'),
+        ],
+      ],
+    ];
+
+    return $build;
+  }
+
+}
